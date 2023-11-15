82de46541d3951cff2de4b27513fa49fe7304e17
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-11-15 19:03:28 +0300

    Add repositories

---
+++ ContactManager.Data/Repositories/Contracts/IContactRepository.cs
@@ -0,0 +1,8 @@
+﻿using ContactManager.Data.Entities;
+
+namespace ContactManager.Data.Repositories.Contracts
+{
+    public interface IContactRepository : IBaseRepository<Contact>
+    {
+    }
+}
---
+++ ContactManager.Data/Repositories/Implementation/ContactRepository.cs
@@ -0,0 +1,24 @@
+﻿using ContactManager.Data.Contexts.Contracts;
+using ContactManager.Data.Entities;
+using ContactManager.Data.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Data.Repositories.Implementation
+{
+    public class ContactRepository : BaseRepository<Contact>, IContactRepository
+    {
+        public ContactRepository(IApplicationDbContext appContext) : base(appContext)
+        {
+        }
+
+        public override async Task<IEnumerable<Contact>> GetAllAsync() =>
+            await appContext.Set<Contact>()
+            .AsNoTracking()
+            .ToListAsync();
+
+        public override async Task<Contact?> GetByIdAsync(int id) =>
+            await appContext.Set<Contact>()
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Id == id);
+    }
+}
