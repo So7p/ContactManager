8e8f410b97586b4ab88755c6b9540e4ee73bb392
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-11-15 19:04:45 +0300

    Configure services extensions

---
+++ ContactManager.Data/IoC/ServicesExtensions.cs
@@ -0,0 +1,32 @@
+﻿using ContactManager.Data.Contexts.Contracts;
+using ContactManager.Data.Contexts.Implementation;
+using ContactManager.Data.Repositories.Contracts;
+using ContactManager.Data.Repositories.Implementation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactManager.Data.IoC
+{
+    public static class ServicesExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("sqlDbConnection")),
+            ServiceLifetime.Transient);
+        }
+
+        public static void ConfigureDbContext(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IContactRepository, ContactRepository>();
+
+            return services;
+        }
+    }
+}
