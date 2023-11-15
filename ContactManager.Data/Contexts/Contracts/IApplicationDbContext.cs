df7e6fe4e84b5e313d7fab141a47e84e16c6becb
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-11-15 19:02:43 +0300

    Add database context

---
+++ ContactManager.Data/Contexts/Contracts/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactManager.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Data.Contexts.Contracts
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Contact> Contacts { get; set; }
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
