4c0939cc2e5faa469bb5a0869aec3386e1e958d3
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-11-15 19:07:08 +0300

    Configure program.cs

--- ContactManager.WebApi/Program.cs
+++ ContactManager.WebApi/Program.cs
@@ -1,16 +1,21 @@
+using ContactManager.Data.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddRepositories();
+
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureDbContext();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -20,8 +25,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
