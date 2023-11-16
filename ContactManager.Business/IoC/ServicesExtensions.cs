d51c55c696a76ebc4308b3c2358dbaf37a4aa91f
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-11-16 13:30:26 +0300

    Configure services extensions

---
+++ ContactManager.Business/IoC/ServicesExtensions.cs
@@ -0,0 +1,35 @@
+﻿using ContactManager.Business.Services.Contracts;
+using ContactManager.Business.Services.Implementation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ContactManager.Business.IoC
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection ConfigureAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureFluentValidation(this IServiceCollection services)
+        {
+            services.AddFluentValidation(opt =>
+            {
+                opt.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IContactService, ContactService>();
+
+            return services;
+        }
+    }
+}
