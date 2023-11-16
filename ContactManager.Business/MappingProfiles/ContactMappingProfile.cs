7d738e25070542de586bbafd49686fa42ab5a21c
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2023-11-16 13:28:15 +0300

    Add mapping profiles

---
+++ ContactManager.Business/MappingProfiles/ContactMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using ContactManager.Business.DTOs.Contact;
+using ContactManager.Data.Entities;
+
+namespace ContactManager.Business.MappingProfiles
+{
+    public class ContactMappingProfile : AutoMapper.Profile
+    {
+        public ContactMappingProfile() 
+        {
+            CreateMap<Contact, ContactForViewDto>()
+                .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => ConvertToDateOnly(src.BirthDate)));
+
+            CreateMap<ContactForCreationDto, Contact>();
+            CreateMap<ContactForUpdateDto, Contact>();
+        }
+
+        private DateOnly ConvertToDateOnly(DateTime dateTime)
+        {
+            var year = dateTime.Year;
+            var month = dateTime.Month;
+            var day = dateTime.Day;
+
+            DateOnly dateOnly = new DateOnly(year, month, day);
+
+            return dateOnly;
+        }
+    }
+}
